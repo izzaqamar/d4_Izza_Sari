69786295e03cd2bbdd129b5389bc9dcf727f599f
Izza Qamar <izzaqamarbci@gmail.com>
2025-11-23 19:36:35 +0100

    Created __init__ for walk to be a package

---
+++ walk/__init__.py
@@ -0,0 +1,10 @@
+# Inf/201 project (D4)
+# Name: Izza Qamar, Sari Ali
+"""
+A package simulating Alex's random walk in Ås.
+This simulation models a student's unsteady walk along a one-dimensional
+representation of Ås, from E6 (coordinate 0) to the railway line (coordinate 100).
+Alex can end up at Pentagon dorms or Kaia dorms depending on the random walk.
+"""
+
+# This file makes walk a Python import package.
